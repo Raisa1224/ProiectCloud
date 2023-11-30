6e82a21823c84404ce347090e97fa4fac201f3ed
Atasie Oana <oana.atasie@s.unibuc.ro>
2023-11-30 16:35:49 +0200

    add initial version for adoption microservice

---
+++ adoption/src/main/java/com/adoption/AdoptionApplication.java
@@ -0,0 +1,13 @@
+package com.adoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionApplication.class, args);
+	}
+
+}
---
+++ adoption/src/test/java/com/adoption/AdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
